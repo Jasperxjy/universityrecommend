40732ec1f02edc2fdfd43fb5b4627c7e8dac3706
Jasperxjy <1662141523@qq.com>
2023-07-14 14:21:48 +0800

    项目初始文件，提交测试

---
+++ src/main/java/com/example/university_recommend/UniversityRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.example.university_recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityRecommendApplication.class, args);
+    }
+
+}
